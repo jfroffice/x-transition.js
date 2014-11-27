d6d867d6c29551880d359f0ac0d414d752b9979d
jfroffice <jfroffice@gmail.com>
2014-11-27 20:14:35 +0100

    initial version

--- dist/x-transition.js
+++ dist/x-transition.js
@@ -1,5 +1,5 @@
 /**
- * x-transition.js - HTML attribute to add dynamic CSS modifier on classes
+ * x-transition.js - HTML attribute to add dynamic CSS classes when transition end
  * @version v0.1.0
  * @link https://github.com/jfroffice/x-transition.js
  * @license MIT
@@ -114,6 +114,48 @@ am.prefix = (function() {
 	return {
 		TRANSITION_END_EVENT: TRANSITION_END_EVENTS[getPrefix('transition')],
 	/*	ANIMATION_END_EVENT: ANIMATION_END_EVENTS[getPrefix('animation')]*/
+	};
+
+})();
+var am = am || {};
+am.transition = (function(undefined) {
+
+	var ATTR = 'x-transition';
+
+	return function() {
+
+		[].forEach.call(document.querySelectorAll('[' + ATTR + ']'), function(element) {
+
+			var cssToAdd = [];
+
+			[].forEach.call(element.attributes, function(attribute) {
+				if (attribute.name.indexOf(ATTR) !== -1) {
+					cssToAdd = attribute.value.split(' ');
+				}
+			});
+
+			if (!cssToAdd.length) {
+				return;
+			}
+
+			var observer = new MutationObserver(function(mutations) {
+				mutations.forEach(function(mutation) {
+					var elm = mutation.target;
+
+					events.one(elm, am.prefix.TRANSITION_END_EVENT, function() {
+						cssToAdd.forEach(function(to) {
+							elm.classList.add(to);
+						});
+					});
+				});
+			});
+
+			observer.observe(element, {
+				attributes: true,
+				childList: true,
+				characterData: true
+			});
+		});
 	};
 
 })();
--- js/core/init.js
+++ js/core/init.js
@@ -1,20 +1,32 @@
 var am = am || {};
 am.transition = (function(undefined) {
 
-	var TO = 'pulse',
-		TO2 = 'animated';
+	var ATTR = 'x-transition';
 
 	return function() {
 
-		[].forEach.call(document.querySelectorAll('[x-transition]'), function(element) {
+		[].forEach.call(document.querySelectorAll('[' + ATTR + ']'), function(element) {
+
+			var cssToAdd = [];
+
+			[].forEach.call(element.attributes, function(attribute) {
+				if (attribute.name.indexOf(ATTR) !== -1) {
+					cssToAdd = attribute.value.split(' ');
+				}
+			});
+
+			if (!cssToAdd.length) {
+				return;
+			}
 
 			var observer = new MutationObserver(function(mutations) {
 				mutations.forEach(function(mutation) {
 					var elm = mutation.target;
 
 					events.one(elm, am.prefix.TRANSITION_END_EVENT, function() {
-						elm.classList.add(TO);
-						elm.classList.add(TO2);
+						cssToAdd.forEach(function(to) {
+							elm.classList.add(to);
+						});
 					});
 				});
 			});
